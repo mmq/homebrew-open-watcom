0ecf87887eca813fd94df6f5e4304718bb78ed4b
Mark Quinn <mmq@mmq.be>
2026-06-22 11:29:52 -0500

    Update open-watcom-v2 to 2026-06-01 build, use dosbox-x, update formula

--- Formula/open-watcom-v2.rb
+++ Formula/open-watcom-v2.rb
@@ -5,7 +5,7 @@ class OpenWatcomV2 < Formula
   version "2.0-2026-06-01"
   sha256 "f876034fae915bcaacce0a4c4d06cd73b4ac91c644127faa416ede9518f56569"
   license "Watcom-1.0"
-  head "https://github.com/open-watcom/open-watcom-v2.git"
+  head "https://github.com/open-watcom/open-watcom-v2.git", branch: "master"
 
   bottle do
     root_url "https://github.com/btb/homebrew-open-watcom/releases/download/open-watcom-v2-2.0-2021-08-01"
@@ -18,20 +18,17 @@ class OpenWatcomV2 < Formula
 
   keg_only "you should use a script to set up your dev environment"
 
-  depends_on "dosbox" => :build
+  depends_on "dosbox-x" => :build
+  depends_on :macos
 
   def install
-    ENV.deparallelize # race conditions in bld/wmake/posmake
-
-    # set the source root
-    inreplace "setvars.sh", "export OWROOT=$(realpath `pwd`)", "export OWROOT=#{buildpath}"
-
-    ENV["OWDOSBOX"] = "dosbox"
-
-    # set the install root
-    ENV["OWRELROOT"] = prefix
-
-    system("./buildrel.sh", "rel")
+    ENV.deparallelize do # race conditions in bld/wmake/posmake
+      inreplace "setvars.sh", 'export OWROOT=$(realpath "`pwd`")', "export OWROOT=#{buildpath}"
+      inreplace "setvars.sh", "export OWTOOLS=GCC", "export OWTOOLS=CLANG"
+      ENV["OWDOSBOX"] = "dosbox-x"
+      ENV["OWRELROOT"] = prefix
+      system("./build.sh", "rel")
+    end
   end
 
   test do
