71f11f8814061cf2618cfe36de428380a2428eb2
Mark Quinn <mmq@mmq.be>
2026-06-22 10:41:38 -0500

    Update open-watcom-v2 to 2026-06-01 build

--- Formula/open-watcom-v2.rb
+++ Formula/open-watcom-v2.rb
@@ -1,14 +1,11 @@
 class OpenWatcomV2 < Formula
   desc "Fork of Open Watcom aimed at 64 bit support"
   homepage "https://github.com/open-watcom/open-watcom-v2/wiki"
+  url "https://github.com/open-watcom/open-watcom-v2/archive/refs/tags/2026-06-01-Build.tar.gz"
+  version "2.0-2026-06-01"
+  sha256 "f876034fae915bcaacce0a4c4d06cd73b4ac91c644127faa416ede9518f56569"
   license "Watcom-1.0"
   head "https://github.com/open-watcom/open-watcom-v2.git"
-
-  stable do
-    url "https://github.com/open-watcom/open-watcom-v2/archive/refs/tags/2021-08-01-Build.tar.gz"
-    version "2.0-2021-08-01"
-    sha256 "3971d6dbbdb859547f7392edd336a1896db8f93f108a2756d69207a38de4cb82"
-  end
 
   bottle do
     root_url "https://github.com/btb/homebrew-open-watcom/releases/download/open-watcom-v2-2.0-2021-08-01"
